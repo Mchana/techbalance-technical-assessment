7b1d09b6536f2e528f5f3950c667a09fc048310e
Mchana <muntejchana@hotmail.com>
2024-04-28 18:19:46 +0100

    settings components

---
+++ components/maincontainer.tsx
@@ -0,0 +1,8 @@
+import { FunctionComponent } from "react";
+import { Text } from "react-native";
+
+const SetttingsTitle: FunctionComponent = () => {
+    return <Text>Hi!</Text>;
+}
+
+export default SetttingsTitle
