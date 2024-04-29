1c759c1dba2f8a64e01c391f465761b60653a3c0
Mchana <muntejchana@hotmail.com>
2024-04-29 09:12:00 +0100

    making logout button

--- App.tsx
+++ App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { StyleSheet, View} from 'react-native';
 import SettingsTitle from './components/settingstitle';
 import Lefticon from './components/lefticon';
+import Logout from './components/logout';
 
 export default function App() {
   return(
     <View style={styles.container}>
       <SettingsTitle></SettingsTitle>
       <Lefticon ></Lefticon>
+      <Logout></Logout>
     </View>
   );
 }
--- components/logout.tsx
+++ components/logout.tsx
@@ -1,8 +1,25 @@
 import React from "react";
-import { Text, StyleSheet, View } from "react-native";
+import { Text, StyleSheet, SafeAreaView, TouchableOpacity } from "react-native";
 
 export default function Logout () {
     return(
-    <View></View>
+    <SafeAreaView style= {styles.container}>
+        <TouchableOpacity>
+        <Text style={{ color: '#000000' }}>Logout</Text>
+        </TouchableOpacity>
+    </SafeAreaView>
     )
 }
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    opacity:{
+        backgroundColor: '#f0f2ff',
+        padding: 10,
+    }
+});
