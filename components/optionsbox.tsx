1ce9c6d83ed5187d534a1aa5431652f8cfaab64f
Mchana <muntejchana@hotmail.com>
2024-04-29 09:48:33 +0100

    creating right icons for box

--- components/optionsbox.tsx
+++ components/optionsbox.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import AntDesign from "@expo/vector-icons/build/AntDesign";
 import { Text, StyleSheet, View } from "react-native";
 
 export default function OptionsBox () {
     return(
     <View style= {styles.container}>
         <Text style={styles.text}>Account</Text>
+        <AntDesign style= {[styles.icon]} name="right" size={24} color="black"/>
         <Text style={styles.text}>Notifications</Text>
         <Text style={styles.text}>Help & Support</Text>
         <Text style={styles.text}>Privacy Policy</Text>
@@ -18,11 +20,12 @@ const styles = StyleSheet.create({
     container:{
         flex: 1,
         display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
+        alignItems: 'flex-start',
+        justifyContent: 'flex-start',
+        flexShrink: 1,
         backgroundColor: '#ffffff',
         borderRadius: 50,
-        width: 300,
+        width: 350,
         height:400,
     },
     opacity:{
@@ -32,8 +35,15 @@ const styles = StyleSheet.create({
         borderRadius: 50,
     },
     text:{
+        flex: 0.8,
+        flexDirection: 'row',
         color: '#000000',
         lineHeight: 32,
         paddingVertical:6,
+        flexWrap:'wrap',
+
+    },
+    icon:{
+        display:'flex',
     }
 });
